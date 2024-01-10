8030cc0f2eadbc8cc642b5c3599cd5bd72c1972f
Sivabalasri <sivabalasri2001@gmail.com>
2024-01-10 11:42:28 +0530

    11.42

--- BoardClass.h
+++ BoardClass.h
@@ -1,21 +1,19 @@
-
 #include <iostream>
 using namespace std;
-
 class Board {
 public:
     int rows;
-    
+    int field [20][20][2] = {{{0}}};
     // Constructor
     Board() : rows(0){}
 
     // Function to display menu and set board dimensions
     void display_menu() {
         // Field options
-        std::cout << "Field Options \tGrid size \t No of Mines:" << std::endl;
-        std::cout << "1. \t\t10-by-10  \t\t12 Mines" << std::endl;
-        std::cout << "2. \t\t15-by-15  \t\t18 Mines" << std::endl;
-        std::cout << "3. \t\t20-by-20  \t\t24 Mines" << std::endl;
+        std::cout << "Field Options \tGrid size \t No of Mines" << std::endl;
+        std::cout << "\t1. \t10-by-10  \t  12 Mines" << std::endl;
+        std::cout << "\t2. \t15-by-15  \t  18 Mines" << std::endl;
+        std::cout << "\t3. \t20-by-20  \t  24 Mines" << std::endl;
 
         // Get user input for field selection
         int selectedOption;
---
+++ check_main_dl.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "BoardClass.h"
+using namespace std;
+
+
+int main()
+{
+	Board myBoard;
+	myBoard.display_menu();
+	cout<<myBoard.rows;
+	return 0;
+}
+
+
